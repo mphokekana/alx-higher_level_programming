e40117073327ee843ae719320cbc22b1ea9d704a
Mpho Kekana <mphokekana736@gmail.com>
2022-11-29 14:09:03 -0800

    Task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,32 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_node - Insert a number into singly-linked list
- * @head: a pointer to insert
- * @number: The number to insert
- * Return: If the function fails - NULL, otherwise - a pointer to the new node.
+ * insert_node - inserts new node
+ * @head: head of singly linked list
+ * @number: value in singly linked list
+ *
+ * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new;
+	listint_t *new_node, *tmp = *head, *tmp2;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (!head)
 		return (NULL);
-	new->n = number;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
 
-	if (node == NULL || node->n >= number)
+	new_node->n = number;
+	if (!tmp || tmp->n >= number)
 	{
-		new->next = node;
-		*head = new;
-		return (new);
+		new_node->next = tmp, *head = new_node;
+		return (new_node);
 	}
 
-	while (node && node->next && node->next->n < number)
-		node = node->next;
+	tmp2 = tmp->next;
+	while (tmp && tmp2 && (tmp2->n < number))
+		tmp = tmp->next, tmp2 = tmp->next;
 
-	new->next = node->next;
-	node->next = new;
-
-	return (new);
+	tmp->next = new_node, new_node->next = tmp2;
+	return (new_node);
 }
