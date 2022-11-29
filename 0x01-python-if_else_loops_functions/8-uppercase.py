52a9a6853ba5a2e4b461c54ca926d4dd43c62140
Mpho Kekana <mphokekana736@gmail.com>
2022-11-29 09:53:16 -0800

    Task 5

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python#
-uppercase = __import__('8-uppercase').uppercase
 
-uppercase("best")
-uppercase("Best School 98 Battery street")
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
