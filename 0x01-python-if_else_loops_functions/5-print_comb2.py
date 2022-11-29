3dacbf86640e84da20240cc1663b7e5f81f627ce
Mpho Kekana <mphokekana736@gmail.com>
2022-11-29 08:56:01 -0800

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 99:
-        print(i)
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number)
     else:
-        print("{:0>2d}".format(i), end=", ")
+        print("{:02}".format(number), end=", ")
