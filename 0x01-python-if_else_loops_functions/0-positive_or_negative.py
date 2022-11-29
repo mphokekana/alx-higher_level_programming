4a0305783129be69f406262de3ced7edc4329da3
Mpho Kekana <mphokekana736@gmail.com>
2022-11-29 06:29:13 -0800

    Task 1

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(-10, 10)
+if num > 0:
+    print(f"{number:d} is posetive")
+elif number == 0:
+    print(f"{number:s} is zero")
+else:
+    print(f"{num,ber:d} is negative")
