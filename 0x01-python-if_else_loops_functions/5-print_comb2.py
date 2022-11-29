6733a89a6089123901863f040cb61c8ab832dcfd
Mpho Kekana <mphokekana736@gmail.com>
2022-11-29 09:21:24 -0800

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 99:
-        print(i)
+for number in range(100):
+    if (number != 99):
+        print("{}{}, ".format(int(number / 10), number % 10), end="")
     else:
-        print("{:0>2d}".format(i), end=", ")
+        print("{}{}".format(int(number / 10), number % 10))
