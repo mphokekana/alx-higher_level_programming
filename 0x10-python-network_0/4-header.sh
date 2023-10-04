2a1ab63a5ad3befe87757540e2cc2d89ac3a1241
Mpho Kekana <111887473+mphokekana@users.noreply.github.com>
2023-10-04 11:52:04 +0000

    Python Network Basics

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # a script that GET and displays response body, adding header variable with parameter
 curl -s "$1" -X GET -H "X-School-User-Id: 98"
+
--- 0x10-python-network_0/README.md
+++ 0x10-python-network_0/README.md
@@ -1,1 +1,2 @@
 Python - Network #0
+
